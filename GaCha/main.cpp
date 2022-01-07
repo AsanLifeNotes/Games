0aa253014eabc8423f379309eb31932043446bf8
root <root@website.asia-east1-b.c.fiery-booth-316208.internal>
2022-01-07 11:16:00 +0800

    save

--- GaCha/main.cpp
+++ GaCha/main.cpp
@@ -1,9 +1,21 @@
+#include <iostream>
+#include <string>
 
-#include "gatcha.h"
+#include "gacha.h"
 
+using namespace std;
 
-int main(int argc,char argv[]){
+int main(int argc,char* argv[]){
 	
+	if(argc>1){
+		for(int i=1;i<argc;i++) cout<<argv[i]<<",";
+		cout<<endl;
+	}else{
+		cout<<"Your Input Is: ";
+		string s;
+		cin>>s;
+		cout<<s<<endl;
+	}
 
 	return 0;
 }
