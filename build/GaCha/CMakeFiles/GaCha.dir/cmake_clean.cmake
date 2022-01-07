44af32fa3a9ae171ecceb32e759633649f5085d4
root <root@website.asia-east1-b.c.fiery-booth-316208.internal>
2022-01-07 22:12:54 +0800

    save

---
+++ build/GaCha/CMakeFiles/GaCha.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GaCha.dir/main.cpp.o"
+  "../../bin/GaCha.pdb"
+  "../../bin/GaCha"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GaCha.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
